dfa7069c2f5a320f85277fff5fe8b270be8f0d92
Serhiy Ivaneyko <ivaneyko.serhiy@gmail.com>
2020-09-16 22:02:03 +0300

    Updates

---
+++ precache-manifest.e62efd7af27c3e786d2c248d1469afa5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "228418d789dbd4a9e06b56ae1a605be8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83a54ab701ba1bd97990",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f4897cee4bc350c5f159",
+    "url": "/static/js/2.f75422a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f75422a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83a54ab701ba1bd97990",
+    "url": "/static/js/main.55a56d6e.chunk.js"
+  },
+  {
+    "revision": "f5db916bff56257f8379",
+    "url": "/static/js/runtime-main.2a28dbf8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
