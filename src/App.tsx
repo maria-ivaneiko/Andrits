dbdf05e971d4d234eb8fc747391643c581eaa14b
Serhiy Ivaneyko <ivaneyko.serhiy@gmail.com>
2020-10-06 16:49:11 +0300

    Add products section and footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 // Layouts
 import Header from 'layouts/Header';
+import Footer from 'layouts/Footer';
 // Pages
 import HomePage from 'pages/Home';
 
@@ -16,6 +17,7 @@ const App:React.FC<IProps> = () => {
       <Switch>
         <Route path="/" component={HomePage} />
       </Switch>
+      <Footer />
     </>
   );
 }
